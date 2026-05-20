da52d31d3282aaa98a7c6ff87119dfd96b63e5f8
fezonV <mr.razor2706@gmail.com>
2026-05-20 18:31:58 +0300

    Добавил методы для Order

--- internal/domain/orderItem.go
+++ internal/domain/orderItem.go
@@ -1,19 +1,20 @@
 package domain
 
 type OrderItem struct {
+	ID        int64
 	ProductID int64
 	Name      string
 	Price     float64
 	Quantity  int
 }
 
-func NewOrderItem(product Product, quantity int) (*OrderItem, error) {
-
+func NewOrderItem(id int64, product Product, quantity int) (*OrderItem, error) {
 	if quantity <= 0 {
 		return nil, ErrInvalidQuantity
 	}
 
 	return &OrderItem{
+		ID:        id,
 		ProductID: product.ID,
 		Name:      product.Name,
 		Price:     product.Price,
